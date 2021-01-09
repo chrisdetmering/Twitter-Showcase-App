11051919f105ec26cf4de2ac076fd2aef2a32282
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-09 17:05:39 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React, { useState, useEffect } from 'react'
+import Header from './components/Header'
+ 
+const App = () => {
+  return(
+    <div>
+      
+
+
+
+      <Header />
+    </div>
+  )
+}
+
+export default App
