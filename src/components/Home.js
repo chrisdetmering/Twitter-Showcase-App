923695e843ab7c342691a59162d8fc8ef902a08e
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-14 16:58:00 -0500

    updated Home.js CSS file

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,13 @@ class Home extends Component {
     render() {
         return (
             <div className="home-container">
-                <h2 className="h2-container">Welcome to the Twitter Showcase!</h2>
-                <p>This is a test</p>
+                <h2 id="home-header">Welcome to the Twitter Showcase!</h2>
+                <p id="home-paragraph">This is a simple web application
+                 that utilizes the Twitter API to retrieve twitter timeline information.
+                 You can search based on user or content in the search page, or utilize 
+                 the random tweet page to pull a random tweet from the selected choices.
+                 </p>
+                <p id="second-home-paragraph">Hope you enjoy!</p>
             </div>
 
         )
