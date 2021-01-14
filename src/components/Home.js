c6fae67ecc77d4c4a9b2aa976ec836c9a78a067c
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-14 13:45:23 -0500

    updated CSS home container

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,13 @@ class Home extends Component {
     render() {
         return (
             <div className="home-container">
-                <h3 className="h3-container">Welcome to the Twitter Showcase!</h3>
+                <h2 className="h3-container">Welcome to the Twitter Showcase!</h2>
+                <p>This is a simple web application that utilizes the Twitter API 
+                to retrieve twitter timeline information. You can search based on user 
+                or content in the search page, or utilize the random tweet page to pull 
+                a random tweet from the selected choices. Hope you enjoy it!</p>
             </div>
+           
         )
     }
 }
