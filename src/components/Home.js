39a6f5704147c3cc7f6d613011dcdb32469fee30
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-14 13:52:48 -0500

    home.js css updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
             <div className="home-container">
                 <h2 className="h3-container">Welcome to the Twitter Showcase!</h2>
                 <div>
-                    <p>Hello it's this is a test</p>
+                    <p>Hello This is a test</p>
                 </div>
             </div>
 
