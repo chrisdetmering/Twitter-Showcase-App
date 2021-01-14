6bc194c0ffcedbec430ba606e69b182f609fd3d6
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-14 13:19:23 -0500

    updated css code

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div className="home-container">
-                <h3>Welcome to the Twitter Showcase!</h3>
+                <h3 className="h3-container">Welcome to the Twitter Showcase!</h3>
             </div>
         )
     }
