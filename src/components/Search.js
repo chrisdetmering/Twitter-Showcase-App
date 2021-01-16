4b1a58e525b184aca5194fd96325925cbd408bc3
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-15 19:50:17 -0500

    added buttons to the search page

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,12 +4,14 @@ class Search extends Component {
     render() {
         return (
             <div>
-                <input  id="input"
+                <input id="input"
                     type="text"
                     placeholder="Search here"
-
-                
                 />
+                <div class="search-buttons">
+                    <button id="button-user">User</button>
+                    <button id="button-content">Content</button>
+                </div>
             </div>
         )
     }
