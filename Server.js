f55dd54c7dea8b881f39ba46c5a5f2855e7273d9
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-20 13:37:32 -0500

    updated code to server.js with API keys

--- Server.js
+++ Server.js
@@ -2,18 +2,28 @@ const express = require('express');
 const server = express();
 const axios = require('axios');
 
+const twitterKey = () => {
+  const API_KEY = "HzSjx6lNrFUYtARRf9dOmaZaK";
+  const API_SECRET_KEY = "DJHuIDsrG5hR7Uy85UbDkcgYsogu8WXrXGcOCcae7aHTFun6Kl";
+  
 
+  
+  
+  server.get("/", (req, res) => {
+    axios.get(`https://api.twitter.com/1.1/search/tweets.json?q=nasa@app_key=${API_KEY}&app_secret_key=${API_SECRET_KEY}`)
+    .then(response => {
+      res.send(response.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  })
+
+  twitterKey();
+} 
 
 //Route
-server.get("/", (req, res) => {
-  axios.get(`https://api.twitter.com/1.1/search/tweets.json?q=nasa`)
-  .then(response => {
-    res.send(response.data)
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-})
+
 
 server.listen(3002, () => {
     console.log("Server is up and listening on 3002...")
