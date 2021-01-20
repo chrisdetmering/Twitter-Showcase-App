62e932f08bfd4b71336a15c6bb1d488df1a7d398
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-20 12:25:49 -0500

    Please enter the commit message for your changes. Lines starting

--- Server.js
+++ Server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const server = express();
+const axios = require('axios');
 
 
 
-// Route
+//Route
 server.get("/", (req, res) => {
-  res.send("heldwdeerrerwwdlo")
+  axios.get(`https://api.twitter.com/1.1/search/tweets.json?q=nasa`)
+  .then(response => {
+    res.send(response.data)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 })
 
 server.listen(3002, () => {
