e8df99913e6e90318f908eaea874e794b38d707c
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-30 15:56:09 -0500

    added names to random.js containers

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -4,11 +4,23 @@ import axios from 'axios';
 const Random = () => {
     return (
         <div>
-            <div class="random-container">Gavriel</div>
-            <div class="random-container">Dan Mason</div>
-            <div class="random-container">Nasa</div>
-            <div class="random-container">Elon Musk</div>
-            <div class="random-container">FIFA</div>
+            <div class="random-container">
+                <p>Gavriel</p>
+            </div>
+            <div class="random-container">
+                <p>Dan Mason</p>
+            
+            </div>
+            <div class="random-container">
+            <p>Nasa</p>
+            </div>
+            <div class="random-container">
+                <p>Elon Musk</p>
+            </div>
+            <div class="random-container">
+                <p>FIFA</p>
+
+            </div>
 
         </div>
     )
