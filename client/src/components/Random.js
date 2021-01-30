ea953668177e253521536937d0daa7ae4d815122
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-30 15:51:58 -0500

    adjusted some more css to random.js

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -1,19 +1,17 @@
 import React from 'react'
-// import axios from 'axios';
+import axios from 'axios';
 
 const Random = () => {
-     return (
-            <div>
-                <div class="random-container">
-                    <div id="random-container-1"></div>
-                    <div id="random-container-2"></div>
-                    <div id="random-container-3"></div>
-                    <div id="random-container-4"></div>
-                    <div id="random-container-5"></div>
-                </div>
-                
-            </div>
-        )
+    return (
+        <div>
+            <div class="random-container">Gavriel</div>
+            <div class="random-container">Dan Mason</div>
+            <div class="random-container">Nasa</div>
+            <div class="random-container">Elon Musk</div>
+            <div class="random-container">FIFA</div>
+
+        </div>
+    )
 }
 
 export default Random
