483ae2190d24c9e27ed79b139f501b4dc5f59c51
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-30 15:01:15 -0500

    adding css to Random.js

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -2,13 +2,16 @@ import React from 'react'
 // import axios from 'axios';
 
 const Random = () => {
-     
-
-        
-
-        return (
+     return (
             <div>
-
+                <div class="random-container">
+                    <div id="random-container-1"></div>
+                    <div id="random-container-2"></div>
+                    <div id="random-container-3"></div>
+                    <div id="random-container-4"></div>
+                    <div id="random-container-5"></div>
+                </div>
+                
             </div>
         )
 }
