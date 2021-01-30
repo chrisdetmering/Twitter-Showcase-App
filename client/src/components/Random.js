dddcbcc9c86cf6997d19bb7f82eb703793243026
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-30 16:41:12 -0500

    attempted to perform a GET request in random.js

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import axios from 'axios';
+import { useState, useEffect } from 'react'
 
 const Random = () => {
+    const [tweets, setTweets] = useState([]);
+   
+
+
+    useEffect(() => {
+       axios.get(`http://localhost:3002/api/tweets/search`)
+            .then(response => {
+                setTweets(response.data)
+                console.log(response.data.statuses)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            
+    })
+
     return (
         <div>
-            
+            {tweets}
 
         </div>
     )
