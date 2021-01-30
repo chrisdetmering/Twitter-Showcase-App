c332ca81438cfda2287a20a7c4068f189fa4dede
Joe Mittiga <joemittiga7@hotmail.com>
2021-01-30 16:01:11 -0500

    deleted css code, will try to retrieve data first

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -4,23 +4,7 @@ import axios from 'axios';
 const Random = () => {
     return (
         <div>
-            <div class="random-container">
-                <p>Gavriel</p>
-            </div>
-            <div class="random-container">
-                <p>Dan Mason</p>
             
-            </div>
-            <div class="random-container">
-            <p>Nasa</p>
-            </div>
-            <div class="random-container">
-                <p>Elon Musk</p>
-            </div>
-            <div class="random-container">
-                <p>FIFA</p>
-
-            </div>
 
         </div>
     )
