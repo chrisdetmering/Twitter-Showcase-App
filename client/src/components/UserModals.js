212cafad51bab955e8d982615286f63f1f0bf8ff
Joe Mittiga <joemittiga7@hotmail.com>
2021-02-16 17:03:22 -0500

    fixed card css

--- client/src/components/UserModals.js
+++ client/src/components/UserModals.js
@@ -6,22 +6,34 @@ const UserModals = ({ userTweet }) => {
         <div class="modals-container">
             <div class="modals">
                 <div class="modal-header">
-                    <div>
-                        <img src={userTweet.user.profile_image_url}/>
+                    <div class="image-container">
+                        <img src={userTweet.user.profile_image_url} />
+                        <div>{userTweet.user.name}</div>
                     </div>
-                    <div>
-                        <div id="modal-name">{userTweet.user.name}  </div>
-                        
+
+
+
+                    <div class="stats-container">
+                        <div><i class="fas fa-heart"></i>{userTweet.favorite_count}</div>
+                        <div><i class="fas fa-retweet"></i>{userTweet.retweet_count}</div>
                     </div>
-                    
+
+
+
+
+
+
+
+
+
+
+
+
                 </div>
-                <div>
-                    <div id="screen-name"> @{userTweet.user.screen_name}</div>
-                </div>
-                
-                
-                <p>{userTweet.full_text}</p>
-                
+                <section>
+
+                    <p>{userTweet.full_text}</p>
+                </section>
             </div>
         </div>
 
