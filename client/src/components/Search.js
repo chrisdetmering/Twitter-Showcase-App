7ac3d970c91c74857d1c69ab47ceb2056ab74671
Joe Mittiga <joemittiga7@hotmail.com>
2021-02-22 15:49:54 -0500

    fixed alert

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -24,8 +24,7 @@ const Search = () => {
             .then((res) => setUserTweets(res.data))
             .catch((err) => console.log(err))
 
-    // input.match(' ') ? alert("Please write an appropriate user handle") : null
-    console.log(userTweets)
+    input.match(' ') ? alert("Please write an appropriate user handle") : null
     }
 
     // Content Tweets
